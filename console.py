b8a4daec01bf712194e30342ac4d583d0edc7a5a
SugiraVinc <sugiravincent11@gmail.com>
2023-02-23 09:11:42 -0800

    console parameters

--- console.py
+++ console.py
@@ -115,6 +115,8 @@ class HBNBCommand(cmd.Cmd):
 
     def do_create(self, args):
         """ Create an object of any class"""
+        print(args)
+        # adding parameters
 
         param = args.split(" ")
         print(param)
@@ -129,7 +131,7 @@ class HBNBCommand(cmd.Cmd):
 
         # list diviion
         x_list = []
-        for x in param:
+        for x in parami_list:
 
             box_list = x.split('=')
             # string evaluation
