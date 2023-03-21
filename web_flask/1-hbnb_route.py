dbd3a427be184bd605743d2279292e5bc3e78c38
root <a.yusuf1@alustudent.com>
2023-03-21 08:12:17 -0700

    code 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -7,26 +7,32 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def index():
-    """returns Hello HBNB!"""
+    """Returns Hello HBNB!"""
     return 'Hello HBNB!'
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
-    """returns HBNB"""
+    """Returns HBNB"""
     return 'HBNB'
+
 
 @app.route('/about')
 def about():
-    """returns an about page"""
+    """Returns an about page"""
     return render_template('about.html')
+
 
 @app.route('/contact')
 def contact():
-    """returns a contact page"""
+    """Returns a contact page"""
     return render_template('contact.html')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
+
 
