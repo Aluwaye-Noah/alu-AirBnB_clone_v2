dcdcafb47c3a31cc701dc408f84964335eaf8b3b
root <a.yusuf1@alustudent.com>
2023-03-21 08:08:04 -0700

    code 1

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -3,20 +3,30 @@
 starts a Flask web application
 """
 
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
 
+app = Flask(__name__)
 
 @app.route('/', strict_slashes=False)
 def index():
     """returns Hello HBNB!"""
     return 'Hello HBNB!'
 
-
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """returns HBNB"""
     return 'HBNB'
 
+@app.route('/about')
+def about():
+    """returns an about page"""
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    """returns a contact page"""
+    return render_template('contact.html')
+
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port='5000')
+    app.run(debug=True)
+
